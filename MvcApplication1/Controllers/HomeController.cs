bad882b4cf28f0b4f1c3524d612e4a5833cf7489
waqarahmad901 <waqarahmad90@gmail.com>
2016-07-28 14:07:15 +0500

    axssc

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,6 +12,30 @@ namespace MvcApplication1.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public string aa(string xml = "test xml")
+        {
+            Stream aa = Request.GetBufferedInputStream();
+           string filename = Server.MapPath("/content/aa.xml");
+            using (Stream file = System.IO.File.Create(filename))
+            {
+                CopyStream(aa, file);
+            }
+            return "welcome";
+        }
+
+        /// <summary>
+        /// Copies the contents of input to output. Doesn't close either stream.
+        /// </summary>
+        public static void CopyStream(Stream input, Stream output)
+        {
+            byte[] buffer = new byte[8 * 1024];
+            int len;
+            while ((len = input.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                output.Write(buffer, 0, len);
+            }
         }
     }
 }
