8394e9f5bb4772da0fd66e1a4341a0002a40f28e
waqarahmad901 <waqarahmad90@gmail.com>
2016-08-10 15:10:00 +0500

    dasdas

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -20,37 +20,44 @@ namespace MvcApplication1.Controllers
 
         public string aa(string aa = "test xml")
         {
-            StreamReader sr = new StreamReader(Request.InputStream);
-            string xml = sr.ReadToEnd();
-          //  xml = "<test>asdasdasdas</test>";
-            string fileName = Server.MapPath("/Content") + "\\" + "aa.xml";
-
-            ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
-            var request = (HttpWebRequest)WebRequest.Create("https://tango.elixir.cz/dspoc/Receiver");
-
-            byte[] bytes;
-            bytes = System.Text.Encoding.ASCII.GetBytes(xml);
-            request.ContentType = "text/xml; encoding='utf-8'";
-            request.ContentLength = bytes.Length;
-            request.Method = "POST";
-         
-            Stream requestStream = request.GetRequestStream();
-            requestStream.Write(bytes, 0, bytes.Length);
-            requestStream.Close();
-            HttpWebResponse response;
-            response = (HttpWebResponse)request.GetResponse();
-            if (response.StatusCode == HttpStatusCode.OK)
+            try
             {
-                Stream responseStream = response.GetResponseStream();
-                string responseStr = new StreamReader(responseStream).ReadToEnd();
-                return responseStr;
-            }
-           // return null;
+                StreamReader sr = new StreamReader(Request.InputStream);
+                string xml = sr.ReadToEnd();
+                //  xml = "<test>asdasdasdas</test>";
+                string fileName = Server.MapPath("/Content") + "\\" + "aa.xml";
 
-            System.IO.File.WriteAllText(fileName, xml);
-           
-            
-            return "Response from service : " +xml;
+                ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
+                var request = (HttpWebRequest)WebRequest.Create("https://tango.elixir.cz/dspoc/Receiver");
+
+                byte[] bytes;
+                bytes = System.Text.Encoding.ASCII.GetBytes(xml);
+                request.ContentType = "text/xml; encoding='utf-8'";
+                request.ContentLength = bytes.Length;
+                request.Method = "POST";
+
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(bytes, 0, bytes.Length);
+                requestStream.Close();
+                HttpWebResponse response;
+                response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream responseStream = response.GetResponseStream();
+                    string responseStr = new StreamReader(responseStream).ReadToEnd();
+                    return responseStr;
+                }
+                // return null;
+
+                System.IO.File.WriteAllText(fileName, xml);
+
+
+                return "Response from service : " + xml;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message + "----------" + ex.StackTrace;
+            }
         }
 
         private bool AcceptAllCertifications(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
